5fec290448406e16f39c0067f25e88bdbae53c43
Nathan Caio <nathan.silva@webjump.com.br>
2022-11-16 16:03:23 -0300

    first upload

---
+++ override-mapping.js
@@ -0,0 +1,18 @@
+module.exports = {
+    '@magento/pagebuilder/lib/config.js': 'src/pagebuilder/lib/config.js',
+
+    '@magento/venia-ui/lib/components/Header/header.js':
+        'src/components/Header/header.jsx',
+
+    '@magento/venia-ui/lib/components/Logo/logo.js':
+        'src/components/Logo/logo.jsx',
+
+    '@magento/venia-ui/lib/components/Header/header.module.css':
+        'src/components/Header/header.module.css',
+
+    '@magento/venia-ui/lib/components/Header/searchTrigger.js':
+        'src/components/Header/searchTrigger.jsx',
+
+    '@magento/venia-ui/lib/components/Header/accountTrigger.js':
+        'src/components/Header/accountTrigger.jsx'
+};
