25c31ca0ca19498d9984b5e5201a54258d1e6606
Nathan Caio <nathan.silva@webjump.com.br>
2022-11-16 18:13:01 -0300

    feature/Header: override default header buttons

--- override-mapping.js
+++ override-mapping.js
@@ -14,5 +14,29 @@ module.exports = {
         'src/components/Header/searchTrigger.jsx',
 
     '@magento/venia-ui/lib/components/Header/accountTrigger.js':
-        'src/components/Header/accountTrigger.jsx'
+        'src/components/Header/accountTrigger.jsx',
+
+    '@magento/venia-ui/lib/components/AccountChip/accountChip.js':
+        'src/components/AccountChip/accountChip.jsx',
+
+    '@magento/venia-ui/lib/components/Header/accountTrigger.module.css':
+        'src/components/Header/accountTrigger.module.css',
+
+    '@magento/venia-ui/lib/components/AccountChip/accountChip.module.css':
+        'src/components/AccountChip/accountChip.module.css',
+
+    '@magento/venia-ui/lib/components/Icon/icon.js':
+        'src/components/Icon/icon.jsx',
+
+    '@magento/venia-ui/lib/components/Icon/icon.module.css':
+        'src/components/Icon/icon.module.css',
+
+    '@magento/venia-ui/lib/components/Header/searchTrigger.module.css':
+        'src/components/Header/searchTrigger.module.css',
+
+    '@magento/venia-ui/lib/components/Header/cartTrigger.js':
+        'src/components/Header/cartTrigger.jsx',
+
+    '@magento/venia-ui/lib/components/Header/cartTrigger.module.css':
+        'src/components/Header/cartTrigger.module.css'
 };
